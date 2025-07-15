22741b2da7b8e57d145adc5db90d73e7e0cba047
Andres Diaz <heladio476@gmail.com>
2025-07-15 10:46:47 -0600

    Gogodev - 12_inertia - Finished

---
+++ 12_inertia/resources/js/Pages/ExampleInertia.vue
@@ -0,0 +1,21 @@
+<!-- Componente de vue necesita de  -->
+<!-- <template></template>
+<script></script>
+<style></style> -->
+
+<template>
+    <h1>Hello world from example!</h1>
+    <h1>{{ counter }}</h1>
+    <button @click="increment">Increment</button>
+    <NavLink :href="route('login')">Login</NavLink>
+</template>
+<script setup>
+import NavLink from '@/Components/NavLink.vue';
+import { ref } from 'vue';
+const counter = ref(0);
+
+const increment = () => {
+    counter.value++;
+}
+</script>
+<style scoped></style>
